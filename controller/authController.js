92623577589bb24e660fbe393b5b430d16e5b086
kaushal <kaushal.kacha02@gmail.com>
2024-09-09 14:49:10 +0530

    Test

--- controller/authController.js
+++ controller/authController.js
@@ -8,19 +8,19 @@ export const ragisterController = async (req, res) => {
     try {
         const { name, email, password, phone, address } = req.body
         if (!name) {
-            return res.send({ error: 'name is required' })
+            return res.send({ message: 'name is required' })
         }
         if (!email) {
-            return res.send({ error: 'email is required' })
+            return res.send({ message: 'email is required' })
         }
         if (!password) {
-            return res.send({ error: 'password is required' })
+            return res.send({ message: 'password is required' })
         }
         if (!phone) {
-            return res.send({ error: 'phone no is required' })
+            return res.send({ message: 'phone no is required' })
         }
         if (!address) {
-            return res.send({ error: 'address is required' })
+            return res.send({ message: 'address is required' })
         }
 
         //check user 
@@ -28,7 +28,7 @@ export const ragisterController = async (req, res) => {
         //existing user 
         if (existingUser) {
             return res.status(200).send({
-                success: true,
+                success: false,
                 message: 'already ragister please login'
             })
         }
